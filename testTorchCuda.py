ba989c390f2c27cd674ba7bbf3b160e8c26aaab7
dalabonba <upup588@gmail.com>
2025-09-08 15:54:18 +0900

    新增pytroch版本提示

--- testTorchCuda.py
+++ testTorchCuda.py
@@ -1,4 +1,10 @@
 import torch
 print(torch.cuda.is_available())
 x = torch.rand(5,3)
-print(x)
+print('PyTorch 版本:', torch.__version__)
+print('PyTorch 綁定的 CUDA 版本:', torch.version.cuda)
+if torch.cuda.is_available():
+    print('實際運行的 CUDA 驅動版本:', torch.version.cuda)
+    print('GPU 型號:', torch.cuda.get_device_name(0))
+else:
+    print('未偵測到可用的 GPU')
